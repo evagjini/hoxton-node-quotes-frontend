f7724a17f51fff6ece14d119e0fd5c6dcf360d8c
Fevronia Gjini <fevronigjini1@gmail.com>
2022-09-01 01:12:31 +0200

    css is a mess

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ type Quotes = {
   id: number;
   author: string;
   quote: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  image: string;
 };
 function App() {
   const [quotes, setQuotes] = useState<Quotes[]>([]);
@@ -21,8 +25,11 @@ function App() {
       <ul>
         {quotes.map((quote) => (
           <li key={quote.id} className="quotes-list">
-            <h2>{quote.author}</h2>
+            <h2 className="name">{quote.firstName}</h2>
+            <h2 className="name">{quote.lastName}</h2>
             <h3 className="description">{quote.quote}</h3>
+            <span> Age : {quote.age}</span>
+            <img className="images" src={quote.image} width="150" />
           </li>
         ))}
       </ul>
